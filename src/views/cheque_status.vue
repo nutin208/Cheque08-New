0d9a2aacf4aab94415701f60a8ce984a5b830766
Pakhanut Pakhakorntanatorn <pakhanut.pakh@mail.kmutt.ac.th>
2020-04-07 15:27:35 +0700

    Testing & add if-else in status

--- src/views/cheque_status.vue
+++ src/views/cheque_status.vue
@@ -2,11 +2,33 @@
   <v-app>
     <v-content>
       <navbar />
-      <h1>Status</h1>
+      <header>
+        <h1>Status</h1>
+        <h6>Staff will check your cheuqe</h6>
+        <div v-if= "Approve">
+          <!-- <img alt="stamp" src="../assets/stamp.png" /> -->
+          A
+        </div>
+        <div v-else-if= "Disqualify">
+          <!-- <img alt="warning" src="../assets/warning.png" /> -->
+          B
+        </div>
+        <div v-else>
+          <!-- <img alt="hourglass" src="../assets/hourglass.png" /> -->
+          C
+        </div>
+      </header>
     </v-content>
   </v-app>
 </template>
-<style></style>
+<style>
+  header {
+    font-size: 2.5em;
+    font-family: "Nunito", sans-serif;
+    text-align: center;
+    margin: 50px auto 25px auto;
+  }
+</style>
 <script>
 import navbar from "@/components/Nav";
 
@@ -17,6 +39,8 @@ export default {
     navbar,
   },
 
-  data: () => ({}),
+  data: () => ({
+    //
+  }),
 };
 </script>
