6fd39fb6643460d10b97f524a2fb9dadf9e1b9a3
Pakhanut Pakhakorntanatorn <pakhanut.pakh@mail.kmutt.ac.th>
2020-04-07 20:10:56 +0700

    Edit some in status

--- src/views/cheque_status.vue
+++ src/views/cheque_status.vue
@@ -4,18 +4,19 @@
       <navbar />
       <header>
         <h1>Status</h1>
-        <h6>Staff will check your cheuqe</h6>
+        
         <div v-if= "Approve">
-          <!-- <img alt="stamp" src="../assets/stamp.png" /> -->
-          A
+          <img alt="stamp" src="../assets/stamp.svg" /><br>
+          Your cheque is approved.
         </div>
         <div v-else-if= "Disqualify">
-          <!-- <img alt="warning" src="../assets/warning.png" /> -->
-          B
+          <img alt="warning" src="../assets/warning.svg" /><br>
+          Your cheque is disqualified.
         </div>
         <div v-else>
-          <!-- <img alt="hourglass" src="../assets/hourglass.png" /> -->
-          C
+          <h6>Staff will check your cheuqe</h6><br>
+          <img alt="hourglass" src="../assets/hourglass.svg" height="100px" width="100px"/><br>
+          Pending ... please waiting.
         </div>
       </header>
     </v-content>
